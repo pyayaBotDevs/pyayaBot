b83a032fc837e9506af133d55273cae6741b0f48
Albinohat <albinohat@gmail.com>
2014-11-23 01:33:12 -0500

    ==userCaseTest==     Removed support for INI configurations.

--- python/pyayaBot_useCaseTest.py
+++ python/pyayaBot_useCaseTest.py
@@ -3,13 +3,13 @@
 
 import pyayaBot_main, sys
 
-if (len(sys.argv) != 3):
-	print "    Syntax error. Usage: pyayaBot_useCaseTest.py channel_name ini|json"
+if (len(sys.argv) != 2):
+	print "    Syntax error. Usage: pyayaBot_useCaseTest.py channel_name"
 	sys.exit()
 
 ## Initialize test variables.
-test_connection_config = "./config/connection_config." + sys.argv[2]
-test_channel_config    = "./config/channel_config_" + sys.argv[1] + "." + sys.argv[2]
+test_connection_config = "../config/connection_config.json"
+test_channel_config    = "../config/channel_config_" + sys.argv[1].lower() + ".json"
 
 ## This int will be turned a list of binary values to control lagging.
 ## INFO Logging    - Add 8
@@ -23,4 +23,4 @@ test_bitmask = 14
 ## Turn the int given into a list of bools.
 test_bitlist = list(bin(test_bitmask)[2:].zfill(4))
 
-test_pyaya   = pyayaBot_main.Bot(test_connection_config, test_channel_config, sys.argv[2], test_bitlist)
+test_pyaya   = pyayaBot_main.Bot(test_connection_config, test_channel_config, test_bitlist)
